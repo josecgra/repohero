560dd29659910a1dd7d31c012c7846395d2b5b7e
Jose Carlos <jcgraucatral@gmail.com>
2021-05-20 20:55:03 +0200

    nueva conexion mysql

--- src/es/eoi/servicios/Conexion.java
+++ src/es/eoi/servicios/Conexion.java
@@ -8,10 +8,15 @@ public class Conexion {
     	
 	private static Conexion instancia;
     private Connection connection;
-    private String url = "jdbc:mysql://db4free.net:3306/hdbcrud?serverTimezone=UTC";
-    private String username = "josecgra";
-    private String password = "josecgra";
 
+	/*
+	 * private String url =
+	 * "jdbc:mysql://db4free.net:3306/hdbcrud?serverTimezone=UTC"; private String
+	 * username = "josecgra"; private String password = "josecgra";
+	 */
+    private String url = "jdbc:mysql://remotemysql.com:3306/LkupNi7lbd?serverTimezone=UTC";
+    private String username = "LkupNi7lbd";
+    private String password = "cNLUoxoMp0";
     private Conexion() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
